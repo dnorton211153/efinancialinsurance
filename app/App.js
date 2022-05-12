80a2a1696c5f2f90e9a00463b7284ecfd13b62ee
Dave Norton <dnorton211153@gmail.com>
2022-05-12 16:41:43 -0600

    bluesky template

--- app/App.js
+++
@@ -1,3 +0,0 @@
-export const App = () => {
-    document.getElementById('app').innerHTML = "Testing";
-}
---
+++ app/views/appView.js
@@ -0,0 +1,50 @@
+/* Norton's rendition of the MVC Property Mgmt Page (June 2019) */
+class AppView {
+
+    constructor(callback) {
+  
+      this.preLoad("https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js", () => {
+        this.preLoad("https://kit.fontawesome.com/48d135817c.js", () => {
+          this.preLoad("https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js", () => {
+            this.preLoad("general.js", () => {
+  
+              // After HB is loaded, get the header
+              fetch("app/views/templates/appHead.html").then(response => response.text()).then(text => {
+                  let t = document.createElement('template');
+                  t.innerHTML = text;
+                  document.head.append(t.content);
+                  callback();
+                });
+              });
+          });
+          
+        })
+      });
+    }
+  }
+  
+  AppView.prototype.render = function(callback) {
+  
+      let messagesPlaceholder = document.createElement("div");
+      messagesPlaceholder.id = "messagesPlaceholder"
+  
+      let mainPlaceholder = document.createElement("div");
+      mainPlaceholder.id = "mainPlaceholder"
+      
+      document.body.appendChild(messagesPlaceholder);
+      document.body.appendChild(mainPlaceholder);
+      callback();
+      
+  };
+  
+  AppView.prototype.preLoad = function (scripting, callback) {
+      var script = document.createElement("script");
+      script.src = scripting;
+      script.addEventListener("load", function() {
+        callback();
+      });
+      document.body.appendChild(script);
+  }
+  
+  export { AppView };
+  
